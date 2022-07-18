49adb5cf03744c014785fe3b33fe96a8427b89d9
ChintanPersonal <chintanpersonal@v1040-wn-rt-a-36-83.campus-dynamic.uwaterloo.ca>
2022-07-18 11:41:01 -0400

    added member-create view

--- team_manager/api/urls.py
+++ team_manager/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MembersList, MemberDetail
+from .views import MembersList, MemberDetail, MemberCreate
 
 urlpatterns = [
     path('', MembersList.as_view(), name='members'),
     path('member/<int:pk>', MemberDetail.as_view(), name='member'),
+    path('member-create/', MemberCreate.as_view(), name='member-create'),
 
 ]
--- team_manager/api/views.py
+++ team_manager/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Member
 
 
@@ -16,9 +19,21 @@ class MembersList(ListView):
    
 
 
-
 #Generic detail view to edit member info
 class MemberDetail(DetailView):
     model = Member
     context_object_name = 'member'
     template_name = 'api/member.html'
+
+
+#add a new team member
+class MemberCreate(CreateView):
+    model = Member
+    
+    #show all fields of the Member class
+    fields = '__all__'
+
+    #after creating a member, redirect to members list
+    success_url = reverse_lazy('members')
+
+    template_name = 'api/member_create.html'
